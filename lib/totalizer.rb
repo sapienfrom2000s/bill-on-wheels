d65ee422e9888b5258b20ed95f1bce794163dd20
brickwall <sk292207@gmail.com>
2024-01-24 21:58:57 +0530

    Use visibility scopes:
    - move methods to private scope

--- lib/totalizer.rb
+++ lib/totalizer.rb
@@ -6,20 +6,6 @@ class Totalizer
     @inventory = inventory
   end
 
-  def regular_total(quantity, unit_price)
-    quantity * unit_price
-  end
-
-  def special_total(sale_unit_quantity, sale_unit_price, quantity, unit_price)
-    units_on_offer = quantity / sale_unit_quantity
-    special_total_on_item = units_on_offer * sale_unit_price
-
-    units_on_regular_price = quantity - (units_on_offer * sale_unit_quantity)
-    regular_total_on_item = regular_total(units_on_regular_price, unit_price)
-
-    special_total_on_item + regular_total_on_item
-  end
-
   def total(cart)
     regular_bill = 0
     final_bill = 0
@@ -47,6 +33,22 @@ class Totalizer
       final: final_bill.round(2),
       savings: (regular_bill - final_bill).round(2),
     }
+  end
+
+  private
+
+  def regular_total(quantity, unit_price)
+    quantity * unit_price
+  end
+
+  def special_total(sale_unit_quantity, sale_unit_price, quantity, unit_price)
+    units_on_offer = quantity / sale_unit_quantity
+    special_total_on_item = units_on_offer * sale_unit_price
+
+    units_on_regular_price = quantity - (units_on_offer * sale_unit_quantity)
+    regular_total_on_item = regular_total(units_on_regular_price, unit_price)
+
+    special_total_on_item + regular_total_on_item
   end
 end
 
