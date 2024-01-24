26e96867be78271a8df1b5e9525e1c640cf34e54
brickwall <sk292207@gmail.com>
2024-01-24 18:56:45 +0530

    Calculate total billing and savings

---
+++ lib/totalizer.rb
@@ -0,0 +1,56 @@
+require_relative 'item'
+require_relative 'sale'
+
+def items
+  milk = Item.new(:Milk, 3.97)
+  bread = Item.new(:Bread, 2.17)
+  banana = Item.new(:Banana, 0.99)
+  apple = Item.new(:Apple, 0.89)
+
+  inventory = [milk, bread, banana, apple]
+end
+
+def input_products
+  puts 'Enter the products seperated by space'
+  products = gets.chomp.split(' ')
+  products.map!(&:capitalize).map!(&:to_sym)
+  products.group_by{|item| item}
+    .transform_values!{|item| item.count}
+end
+
+def total(products)
+  bill_with_discount = 0
+  bill_without_discount = 0
+  items.each do |item|
+    begin
+      quantity = products.fetch(item.name)
+      unit_price = item.price
+    rescue 
+      next
+    end
+
+    begin
+      sale_info = Sale.const_get(item.name.upcase)
+      sale_unit_quantity = sale_info[:quantity]
+      sale_unit_price = sale_info[:price]
+    rescue
+      sale_unit_quantity = nil
+    end
+
+    bill_without_discount += quantity * unit_price
+    unless sale_unit_quantity.nil?
+      bill_with_discount += (sale_unit_price * (quantity/sale_unit_quantity)) + ((quantity%sale_unit_quantity) * unit_price)
+    else
+      bill_with_discount += quantity * unit_price
+    end
+  end
+  {total: bill_with_discount.round(2), savings: (bill_without_discount - bill_with_discount).round(2)}
+end
+
+if __FILE__ == $PROGRAM_NAME
+  products = input_products
+  bill = total(products)
+
+  puts "Your total bill is #{bill[:total]}"
+  puts "You saved #{bill[:savings]} today!"
+end
---
+++ spec/totalizer_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/totalizer'
+require 'spec_helper'
+
+RSpec.describe 'Totalizer' do
+  it 'gives the total bill' do
+    expect(total({
+      :Milk => 3,
+      :Bread => 4,
+      :Apple => 1,
+      :Banana => 1
+      })).to eq({total: 19.02, savings: 3.45})
+  end
+end
