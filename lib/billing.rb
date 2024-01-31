6d6fa035c1d90d31e897e70317f62a35c221b0f0
thirtyone <sk292207@gmail.com>
2024-01-31 10:30:29 +0530

    refactor: extract #item_total from #total

--- lib/billing.rb
+++ lib/billing.rb
@@ -8,6 +8,7 @@ class Billing
   def total(cart)
     regular_bill = 0
     final_bill = 0
+
     @inventory.each do |item|
       begin
         name = item.name
@@ -17,15 +18,10 @@ class Billing
         next
       end
 
-      regular_total_on_item = regular_total(quantity, unit_price)
-      regular_bill += regular_total_on_item
+      total = item_total(item, quantity)
 
-      if item.sale.available
-        # sale = Sale.const_get(name.upcase)
-        final_bill += special_total(item.sale.quantity, item.sale.price, quantity, unit_price)
-      else
-        final_bill += regular_total_on_item
-      end
+      final_bill += (total[:special] || total[:regular])
+      regular_bill += total[:regular]
     end
     {
       final: final_bill.round(2),
@@ -35,17 +31,25 @@ class Billing
 
   private
 
-  def regular_total(quantity, unit_price)
+  def item_total(item, quantity)
+    bill = {regular: nil, special: nil}
+
+    bill[:special] = special_total_on_item(item.sale.quantity, item.sale.price, quantity, item.price) if item.sale.available
+    bill[:regular] = regular_total_on_item(quantity, item.price)
+
+    bill
+  end
+
+  def regular_total_on_item(quantity, unit_price)
     quantity * unit_price
   end
 
-  def special_total(sale_unit_quantity, sale_unit_price, quantity, unit_price)
+  def special_total_on_item(sale_unit_quantity, sale_unit_price, quantity, unit_price)
     units_on_offer = quantity / sale_unit_quantity
     special_total_on_item = units_on_offer * sale_unit_price
 
     units_on_regular_price = quantity - (units_on_offer * sale_unit_quantity)
-    regular_total_on_item = regular_total(units_on_regular_price, unit_price)
 
-    special_total_on_item + regular_total_on_item
+    special_total_on_item + regular_total_on_item(units_on_regular_price, unit_price)
   end
 end
